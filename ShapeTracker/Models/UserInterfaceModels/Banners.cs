6fc4b3a2c833c6925c1a1d66f0de30d77f626011
witherScript <chandlerscott123@gmail.com>
2023-08-28 14:51:13 -0500

    add welcome banner to UIModels + prototypes for functions

---
+++ ShapeTracker/Models/UserInterfaceModels/Banners.cs
@@ -0,0 +1,24 @@
+namespace ShapeTracker.UserInterfaceModels
+{
+  class WelcomeBanner
+  {
+    public static string Welcome {get; } =
+    @"
+  #####                                #######                                           
+ #     # #    #   ##   #####  ######      #    #####    ##    ####  #    # ###### #####  
+ #       #    #  #  #  #    # #           #    #    #  #  #  #    # #   #  #      #    # 
+  #####  ###### #    # #    # #####       #    #    # #    # #      ####   #####  #    # 
+       # #    # ###### #####  #           #    #####  ###### #      #  #   #      #####  
+ #     # #    # #    # #      #           #    #   #  #    # #    # #   #  #      #   #  
+  #####  #    # #    # #      ######      #    #    # #    #  ####  #    # ###### #    # 
+                                                                                         ";
+  }
+  class GoodbyeBanner
+  {
+    //todo
+  }
+  class SubMenuBanner
+  {
+    //todo
+  }
+}
