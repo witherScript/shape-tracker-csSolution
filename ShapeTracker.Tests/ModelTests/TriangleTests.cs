e2d8e385686ce2227ea4ddfb8b91a20c84d764d3
witherScript <chandlerscott123@gmail.com>
2023-08-28 16:50:23 -0500

    second constructor test, private side1

--- ShapeTracker.Tests/ModelTests/TriangleTests.cs
+++ ShapeTracker.Tests/ModelTests/TriangleTests.cs
@@ -9,8 +9,19 @@ namespace ShapeTracker.Tests
     [TestMethod]
     public void TriangleConstructor_Creates_InstanceOfTriangle_Triangle()
     {
-      Triangle newTriangle = new Triangle();
+      Triangle newTriangle = new Triangle(2);
       Assert.AreEqual(typeof(Triangle), newTriangle.GetType());
+    }
+
+    [TestMethod]
+    public void GetSide1_ReturnsSide1_Int()
+    {
+      int length1 = 3; //Arrange
+      Triangle newTriangle = new Triangle(length1);
+
+      int result = newTriangle.Side1; //Act
+
+      Assert.AreEqual(length1, result); //Assert
     }
   }
 
