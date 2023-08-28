59d33c2dc507770d91dccda87458405839ebd963
witherScript <chandlerscott123@gmail.com>
2023-08-28 16:27:10 -0500

    boilerplate for testing Triangle.cs

---
+++ ShapeTracker.Tests/ModelTests/TriangleTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShapeTracker.Models;
+
+namespace ShapeTracker.Tests
+{
+  [TestClass]
+  public class TriangleTests
+  {
+    //todo
+  }
+
+}
