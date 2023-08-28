5e55dfb1e5b772ade81bedf165a211d5647c2846
witherScript <chandlerscott123@gmail.com>
2023-08-28 16:51:06 -0500

    passing impl for GetSide1

--- ShapeTracker/Models/Triangle.cs
+++ ShapeTracker/Models/Triangle.cs
@@ -4,53 +4,16 @@ namespace ShapeTracker.Models
 {
   public class Triangle
   {
-    //public int Side2 { get; set; }
-    public int Side1 {get; set;}
-    public int Side2 {get; set;}
-    public int Side3 {get; set;}
-    public static List<Triangle> Instances { get; set; } = new List<Triangle> {};
+    private int _side1;
+    public int Side1
+    {
+      get {return _side1;}
+    }
+    public Triangle(int length1)
+    {
+      _side1 = length1;
 
-    public Triangle(int length1, int length2, int length3)
-  {
-    Side1 = length1;
-    Side2 = length2;
-    Side3 = length3;
-    _instances.Add(this);
-  }
-  
-  public string CheckType()
-  {
-    if(Side1 == Side2 && Side2 != Side3 || Side1 == Side3 && Side3 != Side2)
-    {
-      return "Isosceles";
-    }
-    else if (Side1==Side2 && Side2 ==Side3)
-    {
-      return "Equilateral";
-    }
-    else if(Side1 != Side2 && Side2!=Side3 && Side1!=Side3)
-    {
-      return "Scalene";
-    }
-    else if ((Side1 > (Side2 + Side3)) || (Side2 > (Side1 + Side3)) || (Side3 > (Side1 + Side2)))
-    {
-      return "not a triangle";
-    }
-    else{
-      return "An Unknown error occurred";
     }
   }
-
-  public static List<Triangle> GetAll()
-  {
-    return _instances;
-  }
-
-  public static void ClearAll()
-  {
-    _instances.Clear();
-  }
-
-}
   
 }
